9d81b9d66002e6015e40b8438203da7528991c37
bharathi13 <sathyabharathi00131@gmail.com>
2021-05-17 20:37:37 +0530

    today i found out i can't download videos in kissanime with a desiered quality so i am working in gogoanime

--- providers/anime.py
+++ providers/anime.py
@@ -16,6 +16,8 @@ class anime:
         "item_movies_in_cat item_movies_in_cat_thumb", 
         "item_movies_in_cat item_movies_in_cat_thumb odd"
     )
+    gogo_main_url = 'https://www1.gogoanime.ai/'
+
 
     def __init__(
         self,
@@ -41,6 +43,20 @@ class anime:
                 search_url = self.search_url_list[0] + self.anime_name
                 self.search_anime_scr(search_url=search_url)
 
+                anime_scr = self.soup.find('ul', class_='items')
+
+                for anime_name_scr in anime_scr.find_all('li'):
+
+                    self.anime_name = str(anime_name_scr.find('p', class_='name').text)
+                    anime_release = str(anime_name_scr.find('p', class_='released').text).strip('\n')
+                    self.anime_release = anime_release.strip(' ')
+
+                    anime_link_scr = str(anime_name_scr.find('a'))
+                    part_of_the_link = anime_link_scr.split('"')[1]
+                    self.anime_link = self.gogo_main_url+part_of_the_link
+
+                    print(self.anime_name+'____   '+self.anime_release+' - '+self.anime_link,'\n')
+
             if self.website == "kissanime":
                 search_url = self.search_url_list[1] + self.anime_name
                 self.search_anime_scr(search_url=search_url)
@@ -53,7 +69,6 @@ class anime:
 
                     self.anime_link_scr = str(self.anime_name_link.a).split('"')
                     self.anime_link = self.anime_link_scr[-2]
-
 
                     self.anime_status = str(anime_list_scr.find('div', class_='status_in_cat_container').text).strip('\n')
 
@@ -68,29 +83,31 @@ class anime:
 
     def list_all_ep(self, ep_link='none'):
 
-        self.ep_link = ep_link
+        self.ep_link_main = ep_link
 
-        url = requests.get(self.ep_link).text
+        url = requests.get(self.ep_link_main).text
         soup = bs(url, 'lxml')
 
-        ep_list_scr = soup.find('div', class_="listing listing8515 full").h3
-        
-        ep_name =  str(ep_list_scr.text).strip('\n')
+        if self.website == 'kissanime':
 
-        ep_link_scr  = str(ep_list_scr).split('"')
-        ep_link = ep_link_scr[3]
+            for ep_list_all in soup.find_all('div', class_="listing listing8515 full"):
 
-        ep_id_scr = ep_link[3].split('id')
+                for self.ep_list_scr in ep_list_all.find_all('h3'):
+                
+                    self.ep_name =  str(self.ep_list_scr.text).strip('\n')
 
+                    ep_link_scr  = str(self.ep_list_scr).split('"')
+                    self.ep_link = ep_link_scr[3]
 
-        
-        print(ep_id_scr)
+                    ep_id_scr = self.ep_link.split('=')
+                    self.ep_id = ep_id_scr[-1]
 
-        
+                    print(self.ep_name+'___'+self.ep_link+'  -  '+self.ep_id,'\n')
+            
 
 if __name__ == "__main__":
-    bleach = anime("full metal", website="kissanime")
+    bleach = anime("bleach", website="gogoanime")
 
-    bleach.list_all_ep(ep_link='https://kissanime.com.ru/Anime/fullmetal-alchemist-dub.25384/')
+    # bleach.list_all_ep(ep_link='https://kissanime.com.ru/Anime/fullmetal-alchemist-dub.25384/')
 
-    # anime.list_all_ep(ep_link='https://kissanime.com.ru/Anime/fullmetal-alchemist-dub.25384/')
+    bleach.search_anime()
